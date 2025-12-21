f2d4cb0a6fa64dad9344cb51410edd87717fd051
Mir <meerrahimblue@gmail.com>
2025-12-22 01:10:28 +0300

    db config & tools  setup

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,10 +1,13 @@
 import express from "express";
 import path from "path";
 import { ENV } from "./config/env.js";
+import { connectDB } from "./config/db.js";
+import { clerkMiddleware } from "@clerk/express";
 
 const app = express();
 const __dirname = path.resolve();
 const PORT = process.env.PORT || 3000;
+app.use(clerkMiddleware());
 
 app.get("/api/health", (req, res) => {
   res.status(200).json({ message: "OK" });
@@ -23,4 +26,5 @@ if (ENV.NODE_ENV === "production") {
 
 app.listen(ENV.PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
+  connectDB();
 });
